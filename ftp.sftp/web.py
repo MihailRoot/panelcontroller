a9e7779f2cc6bdfc5237f9d5157a2be50b893941
MihailC <47941033+MihailRoot@users.noreply.github.com>
2022-08-12 12:49:04 +0300

    non super update(test ftp on python)

--- ftp.sftp/web.py
+++ ftp.sftp/web.py
@@ -2,11 +2,24 @@ import time
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
-import websocket
-auth = DummyAuthorizer()
-auth.add_user("Userhere","Password","Dir",perm='')
-handler  = FTPHandler
-handler.authorizer = auth;
+import requests
+from threading 
 
-server = FTPServer(("0.0.0.0",21),handler)
-server.serve_forever()
+class Ftp(threading.Thread):
+    def __init__(self,req):
+        super().__init__()
+        self.req = req
+    def run(self):
+
+        auth = DummyAuthorizer()
+        auth.add_user(f'{self.req["email"]}',f'{self.req["name"]}',"/",perm='')
+        handler  = FTPHandler
+        handler.authorizer = auth
+
+        server = FTPServer(("0.0.0.0",21),handler)
+        server.serve_forever()
+        
+get = requests.get('http://20.82.177.87:5062/Servers/Detailsapi/1')
+json = get.json()
+th = Ftp(json)
+th.start()
